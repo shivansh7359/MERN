8685f07806c6c3652b2b2264ebe85e13431b6f60
Shivansh Singhal <yeager9336@gmail.com>
2023-07-18 10:43:57 +0530

    again adding variables.js

---
+++ variables.js
@@ -0,0 +1,23 @@
+var x;
+console.log(x);
+
+
+let y = 20;
+console.log(y);
+
+var a;
+var b;
+
+let temp;
+
+const c=122;
+// c = 22;
+console.log(c);
+
+var itsNull = null;
+console.log(itsNull + 5);
+
+
+
+
+
